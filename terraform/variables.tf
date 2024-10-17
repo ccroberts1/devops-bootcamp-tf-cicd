2037c3ab295ba2bc0d78e5f2e2e3719686731fb7
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-10-17 15:41:36 -0600

    Add tf files and update Jenkinsfile to include tf commands

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+  default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default = "10.0.10.0/24"
+}
+variable avail_zone {
+  default = "us-east-1a"
+}
+variable env_prefix {
+  default = "dev"
+}
+variable my_ip {
+  default = "71.205.44.240/32"
+}
+variable jenkins_ip {
+  default = "98.81.243.117/32"
+}
+variable instance_type {
+  default = "t2.micro"
+}
+variable region {
+  default = "us-east-1"
+}
