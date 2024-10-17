2e182e9b88571cf5a5e08210b4d235cd3f6aef66
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-10-17 16:07:23 -0600

    Fix issue with docker pwd throwing weird character issues

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage(String imageName) {
     echo "building the docker image: $imageName"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t $imageName ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo '$PASS' | docker login -u $USER --password-stdin"
         sh "docker push $imageName"
     }
 }
