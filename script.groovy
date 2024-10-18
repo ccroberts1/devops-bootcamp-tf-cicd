121da7fc087fe0f4421edcb637c0a796f723c26b
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-10-18 10:37:03 -0600

    Uncomment buidImage section of groovy script

--- script.groovy
+++ script.groovy
@@ -5,11 +5,11 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image"
-//    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//        sh 'docker build -t ccroberts1/demo-app:java-maven-2.0 .'
-//        sh 'echo $PASS | docker login -u $USER --password-stdin'
-//        sh 'docker push ccroberts1/demo-app:java-maven-2.0'
-//    }
+    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ccroberts1/demo-app:java-maven-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push ccroberts1/demo-app:java-maven-2.0'
+    }
 }
 
 return this
